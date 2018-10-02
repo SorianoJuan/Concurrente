6b8cfffb26b0511532b8ca91c078280a78f0257b
ivanvig <ivanmvig@gmail.com>
2018-10-01 23:46:20 -0300

    WiP: Monitor

--- src/Monitor.java
+++ src/Monitor.java
@@ -3,49 +3,45 @@ import java.util.Collections;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import Transition
 
 public class Monitor{
-    private PetriNet pnet;
+    private PetriNet PNet;
 
     private Condition[] ConditionQueue;
-    private Integer[] ConditionQueueN;
-
     private Condition CortesyQueue;
-    private int CortesyQueueN;
-
-    //private Condition InputQueue; No haria falta?
-    //private int InputQueueN;
-
 
     private Lock lock;
-    //private politica
 
-    public Monitor(PetriNet pnet){
-        this.pnet = pnet;
+    public Monitor(PetriNet PNet){
+        this.PNet = PNet;
 
         this.lock = new ReentrantLock();
 
-        this.ConditionQueue = new Condition[this.pnet.getAmountTransitions()];
+        this.ConditionQueue = new Condition[this.PNet.getAmountTransitions()];
         for(int i = 0; i < this.ConditionQueue.size(); i++) this.ConditionQueue[i] = lock.newCondition();
-        this.ConditionQueueN = new Integer[this.pnet.getAmountTransitions()]; //TODO: rellenar con 0
 
         this.CortesyQueue = lock.newCondition();
-        this.CortesyQueueN = 0;
-
-        //this.InputQueue = lock.newCondition();
-        //this.InputQueueN = 0;
     }
-    public void exec(ArrayList<Integer> tlist){
-        synchronized(pnet){
-            //TODO: convertir vector
-            trans = this.calculate(tlist); //calculate es metodo de RdP o monitor?
-            if(trans != 0){
-                pnet.disparar(trans);
-            }else{
-                this.encolar();
-            }
-
+    public void exec(Transicion t){
+        this.lock.lock();
+        //TODO: implementar politica
+        Transition next_t = this.PNet.getNextTransition();
+        if(t.getId() == next_t.getId){
+            this.PNet.trigger(t);
+        }else{
+            this.ConditionQueue[next_t.getId()].signal();
+            this.ConditionQueue[t.getId()].await();
         }
+        // while(trans == 0){
+            
+        // }
+        // if(trans != 0){
+        //     this.pnet.disparar(trans);
+        // }else{
+        //     this.encolar();
+        // }
+        
     }
 
 }
