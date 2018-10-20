a51a9ead7d54ff5f2af8651ffa1203ae7f98b142
ivanvig <ivanmvig@gmail.com>
2018-10-20 19:54:31 -0300

    metodo para suma de matrices

---
+++ src/Matrix.java
@@ -0,0 +1,24 @@
+package concurrency;
+
+public final class Matrix{
+    public static int[][] sum(int[][] a, int[][] b){
+        if(a[0].length != b[0].length || a.lenght != b.length){
+            throw new ArithmeticException
+                (
+                 "No se puede sumar matrices de diferentes dimensiones."
+                 )
+        }
+
+        int row_size = a.length;
+        int column_size = a[0].length;
+
+        int[row_size][column_size] result;
+
+        for(int i=0; i<row_size; i++){
+            for(int j=0; i<column_size; j++){
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        return result;
+    }
+}
