9cd35564184087749c08c85733ce4455f028aa83
ivanvig <ivanmvig@gmail.com>
2018-11-17 11:51:14 -0300

    Task verbose

--- src/main/Task.java
+++ src/main/Task.java
@@ -6,24 +6,31 @@ import java.util.concurrent.TimeUnit;
 public class Task implements Runnable{
     private Monitor mon;
     private Transition[] tarray;
-    private boolean keepGoing;
+    private int verbose;
 
     public Task(int[] indexs, PetriNet pnet, Monitor mon){
         this.tarray = Arrays.stream(indexs).mapToObj(pnet.getTransitionList()::get).toArray(Transition[]::new);
         this.mon = mon;
-        this.keepGoing = true;
     }
 
     public Transition[] getTransitionArray(){
         return this.tarray;
     }
+    public void setVerboseLevel(int lvl){this.verbose = lvl;}
 
     @Override
     public void run(){
-        while(this.keepGoing){
+        boolean keepGoing = true;
+        while(keepGoing){
             for(Transition t: this.tarray){
-                this.keepGoing = this.mon.exec(t);
+                keepGoing = this.mon.exec(t);
             }
         }
+        if(verbose > 0)
+            System.out.println
+                (
+                 "Msg From:"+ Thread.currentThread().getName()+
+                 "\nThread terminado"
+                 );
     }
 }
